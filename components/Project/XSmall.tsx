bd7643f99a50e6c325f2e5d4ea4e3aa3f30dea95
idiom-bytes <69865342+idiom-bytes@users.noreply.github.com>
2023-02-05 11:51:12 -0800

    updating project component.

--- components/Project/XSmall.tsx
+++ components/Project/XSmall.tsx
@@ -5,9 +5,14 @@ const XSmall = ({ project }) => {
   return project ? (
     <a href={project.url} target="_blank" rel="noreferrer">
       <div className="">
-        <a className="group block sm:cursor-pointer transition duration-200 h-60 w-full rounded-xl bg-gray-100 focus-within:ring-2 relative focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden border border-grey-light dark:border-black">
-          <div className="absolute h-full w-full font-semibold px-4 pb-2  text-lg flex items-end gradient text-white">
-            {project.title}
+        <a className="group block sm:cursor-pointer transition duration-200 h-60 w-full rounded-xl bg-gray-100 focus-within:ring-5 relative focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden border border-grey-light dark:border-black">
+          <div className="absolute h-full w-full gradient">
+            <div className="absolute top-0 font-semibold px-4 pb-2 text-lg text-white">
+              {project.title}
+            </div>
+            <div className="absolute bottom-0 font-semibold px-4 pb-2 text-sm text-white">
+              {project.description}
+            </div>
           </div>
           <img
             className="object-cover w-full h-full"
