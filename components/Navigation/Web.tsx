0a85912c6ef93727cadb5887965fe11f1bfb67a3
idiom-bytes <69865342+idiom-bytes@users.noreply.github.com>
2023-03-14 18:50:31 -0700

    Updating urls, footer, and a few others.

--- components/Navigation/Web.tsx
+++ components/Navigation/Web.tsx
@@ -94,7 +94,7 @@ const Web = () => {
                 </div>
 
                 <div className="flex items-center flex-shrink-0">
-                  <Button link="https://nouns.wtf/" text="nouns.wtf" small />
+                  <Button link="https://www.oceannouns.wtf/" text="Coming Soon..." small />
                 </div>
               </div>
             </div>
